a31cc58494ee5ff808504b0ab4e692d709a8269e
drory hagai <hagayd@eitantest2.cs.bgu.ac.il>
2011-11-16 17:47:17 +0200

    bfix comlete

--- lab3/bfix/bfix.c
+++ lab3/bfix/bfix.c
@@ -2,15 +2,54 @@
 #include <stdio.h>
 #include "diff.h"
 
+#include <unistd.h>
+#include <getopt.h>
+
+
 int main(int argc, char **argv){
-    
-	if (argc < 3){
+	int help, reverse, message;
+	help = 0;
+	reverse = 0;
+	message = 0;
+	int opt;
+	char *optstring = "hrm";
+	int i;
+
+	
+	for(opt = getopt(argc, argv, optstring); opt != -1; opt = getopt(argc, argv, optstring)){
+		switch(opt){
+		  case 'r':
+			reverse =1;
+			break;
+		  case 'h':
+			help = 1;
+			break;
+		  case 'm':
+			message = 1;
+			break;
+		  default:
+			printf("unknown option. Exiting...\n",opt);
+			return 0;
+		}
+	}
+	
+
+	if (argc-optind != 2){
 		printf("USAGE: [OPTIONS] file differences\n",argv[0]);
 		return 1;
 	}
 	
-	char *filename = argv[1];
-	char *filename_diff = argv[2];
+	if(help ==1){
+		printf("USAGE: [OPTIONS] file differences\n",argv[0]);
+		printf("OPTIONS\n");
+		printf("-h  print a summary of options and exit\n");
+		printf("-r  reverse the differences\n");
+		printf("-m  print a message each time a change is applied\n");
+		return 0;
+	}
+	
+	char *filename = argv[optind];
+	char *filename_diff = argv[optind+1];
 	FILE *fileStream = fopen(filename, "rb+");
 	FILE *fileStream_diff = fopen(filename_diff, "rb+");
 	
@@ -23,21 +62,7 @@ int main(int argc, char **argv){
 	byteNum = 1;
 	not_endOfFile = 1;
 	struct diff diff;
-	
-	
-	while ((not_endOfFile == 1)){
-		printf("debug: reading byte %ld\n",byteNum);
-		fflush(stdout);
-		not_endOfFile = fread((char *)&c, sizeof(char), 1, fileStream);
 
-		if (not_endOfFile == 0){
-			printf("debug: end of file\n");
-			not_endOfFile = 0;
-		}
-		printf("debug: byte %ld in file is %u\n",byteNum, c);
-		byteNum ++;
-	}
-	
 	int LINE_LENGTH = 80;
 	
 	char line[LINE_LENGTH];
@@ -45,38 +70,32 @@ int main(int argc, char **argv){
 	
 	//read the lines in diff file
 	while(fgets(line, LINE_LENGTH, fileStream_diff) != NULL ){
-			//for each line
-			printf("debug: reading line %d:%s\n",lineNum,line);
-			fputdiff(stdout, parsediff(line, &diff));
+			parsediff(line, &diff);
 			lineNum++;
 			//check if the file has the old value
 			fseek(fileStream, diff.offset-1, SEEK_SET);
 			fread((char *)&c, sizeof(char), 1, fileStream);
-			printf("debug: byte at offset %ld  is %u\n",diff.offset, c);
-			printf("debug: old byte at offset %ld  is %u\n",diff.offset, diff.old);
-			printf("debug: new byte at offset %ld  is %u\n",diff.offset, diff.new);
+
+			if (reverse == 1){
+			  int temp;
+			  temp = diff.old;
+			  diff.old = diff.new;
+			  diff.new = temp;
+			}
 			if(c == diff.old)
 			{
 				
 				fseek(fileStream, diff.offset-1, SEEK_SET);
 				fwrite((char *)&diff.new, sizeof(char), 1, fileStream);
-				printf("debug: value has changed\n");
+				if(message == 1){
+					printf("setting byte at offset %ld from %u to %u\n",diff.offset, diff.old, diff.new);
+				}
 			  
 			}
 			else{
-				printf("debug: old value doesn't much\n");
+				printf("old value doesn't much\n");
 			}
 		}
-	
-	
-	//testing
-	/*
-	long offset;
-	offset = 2;
-	fseek(fileStream, offset, SEEK_SET);
-	fread((char *)&c, sizeof(char), 1, fileStream);
-	printf("debug: byte at offset %ld  is %u\n",offset+1, c);
-	*/
 	
 	
 	fclose(fileStream);
