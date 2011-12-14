6dbefeb2acd89b025bdabf9cbeaad7bfcdd9e14c
Hagai Drori <drory.hagai@gmail.com>
2011-12-14 12:27:55 +0200

    printing semi works

--- lab5/bcmp/asm_bcmp.c
+++ lab5/bcmp/asm_bcmp.c
@@ -9,7 +9,7 @@
  */
 
 unsigned int myopen(char*);
-void myreadSingleChar(unsigned int, char[]);
+int myreadSingleChar(unsigned int, char[]);
 void printFirstChar(char*);
 void myclose(unsigned int);
 void sys_puts();
@@ -20,51 +20,27 @@ int asm_bcmp(int argc, char *argv[]){
 		return 1;
 	}
 
-	sys_puts(argv[1]);
-	sys_puts(argv[2]);
-	
+	char *nl = "\n";	
+
 	char buf2[1];
 	char buf1[1];
-	
-	
-	
+		
 	char *filename1 = argv[1];
 	char *filename2 = argv[2];	
 	unsigned int fileHandler1 = myopen(filename1);
 	unsigned int fileHandler2 = myopen(filename2);
 
-	myreadSingleChar(fileHandler1, buf1);
-	myreadSingleChar(fileHandler2, buf2);
-	if ((char)*buf1 == (char)*buf2){
-		char* msg = "true\n";
-		sys_puts(msg);
-	}
-	else{
-		char* msg = "false\n";
-		sys_puts(msg);
-	}
-	//sys_puts(buf1);
-	sys_puts(buf2);
 
-
-	myclose(fileHandler1);
-	myclose(fileHandler2);
-
-/*
-	char *filename1 = argv[1];
-	char *filename2 = argv[2];
-	
-	//FILE *fileStream1 = fopen(filename1, "rb");
-	//FILE *fileStream2 = fopen(filename2, "rb");
-	
-	
-	int fileHandler1 = open(filename1);
-	int fileHandler2 = open(filename1);
 	// contiue reading the file until a diffrence is found or end of file reached
 	int diff, endOfFile1, endOfFile2;
 	char f1=0;
 	char f2=0;
 	int byteNum;
+
+	int diff_abc;
+	char temp1 = '1';
+	char temp2 = 1;
+	diff_abc = temp1-temp2;
 
 	byteNum = 1;
 	diff = 0;
@@ -74,26 +50,34 @@ int asm_bcmp(int argc, char *argv[]){
 
 	// read the files until a diffrence is found 
 	while ((endOfFile1 == 1) & (endOfFile2 == 1)){
-		//printf("debug: reading byte %d\n",byteNum);
-		fflush(stdout);
-		endOfFile1 = fread((char *)&f1, sizeof(char), 1, fileStream1);
-		endOfFile2 = fread((char *)&f2, sizeof(char), 1, fileStream2);
+		endOfFile1 = myreadSingleChar(fileHandler1, buf1);
+		endOfFile2 = myreadSingleChar(fileHandler2, buf2);
 
 		if ((endOfFile2 == 0) | (endOfFile1 == 0)){
 			//printf("debug: end of file\n");
 			break;
 		}
-		if (f1 != f2){
-			printf("byte %d -%u +%u\n",byteNum, f1, f2);
+		if ((char)*buf1 != (char)*buf2){
+			char* msg = "byte ";
+			sys_puts(msg);
+			//*(msg+4) = byteNum + diff_abc;
+			//sys_puts(msg+4);
+			msg = " -";
+			sys_puts(msg);
+			sys_puts(buf1);
+			msg = " +";	
+			sys_puts(msg);
+			sys_puts(buf2);
+			sys_puts(nl);
+			//printf("byte %d -%u +%u\n",byteNum, f1, f2);
 			diff = 1;
 		}
 		//printf("debug: byte %d in file1 is %d & in file2 is %d\n",byteNum, f1, f2);
 		byteNum ++;
 	}
 	
+	myclose(fileHandler1);
+	myclose(fileHandler2);
 
-	fclose(fileStream1);
-	fclose(fileStream2);
-*/
 	return 0;
 }
--- lab5/bcmp/myreadSingleChar.asm
+++ lab5/bcmp/myreadSingleChar.asm
@@ -8,6 +8,7 @@ myreadSingleChar:					;void myreadSingleChar(fd, buf)
 	mov	ebx,[esp + 8]	;file descriptor
 	mov	ecx,[esp + 12]	;buf
 	mov word edx,1		;size to read
+	mov	eax,3		;system call number (sys_read)
 	int	0x80		;call kernel
 	movzx ecx, cl		;buf is only a char
 
